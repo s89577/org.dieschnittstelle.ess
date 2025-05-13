5e66e11a06e274d46a86d34e9c0c9e19313ea2f3
LAPTOPNADJA\nadja <s89577@bht-berlin.de>
2025-05-13 13:31:40 +0200

    JSR2 und 3

--- org.dieschnittstelle.ess.jrs/src/main/java/org/dieschnittstelle/ess/jrs/ProductCRUDServiceImpl.java
+++ org.dieschnittstelle.ess.jrs/src/main/java/org/dieschnittstelle/ess/jrs/ProductCRUDServiceImpl.java
@@ -1,5 +1,13 @@
 package org.dieschnittstelle.ess.jrs;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Context;
+import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.entities.GenericCRUDExecutor;
+import org.dieschnittstelle.ess.entities.crm.StationaryTouchpoint;
+import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
 
 import java.util.List;
@@ -10,36 +18,57 @@ import java.util.List;
 
 public class ProductCRUDServiceImpl implements IProductCRUDService {
 
-	@Override
-	public IndividualisedProductItem createProduct(
-			IndividualisedProductItem prod) {
-		// TODO Auto-generated method stub
-		return null;
+	protected static Logger logger = org.apache.logging.log4j.LogManager.getLogger(ProductCRUDServiceImpl.class);
+
+	/**
+	 * this accessor will be provided by the ServletContext, to which it is written by the ProductServletContextListener
+	 */
+	private GenericCRUDExecutor<AbstractProduct> productCRUD;
+
+	/**
+	 * here we will be passed the context parameters by the resteasy framework. Alternatively @Context
+	 * can  be declared on the respective instance attributes. note that the request context is only
+	 * declared for illustration purposes, but will not be further used here
+	 *
+	 * @param servletContext
+	 */
+	public ProductCRUDServiceImpl(@Context ServletContext servletContext, @Context HttpServletRequest request) {
+
+		this.productCRUD = (GenericCRUDExecutor<AbstractProduct>) servletContext.getAttribute("productCRUD");
+
+		logger.debug("read out the productCRUD from the servlet context: " + this.productCRUD);
 	}
 
 	@Override
-	public List<IndividualisedProductItem> readAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+	public AbstractProduct createProduct(
+			AbstractProduct prod) {
+		return (AbstractProduct) this.productCRUD.createObject(prod);
 	}
 
 	@Override
-	public IndividualisedProductItem updateProduct(long id,
-			IndividualisedProductItem update) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<AbstractProduct> readAllProducts() {
+		return (List) this.productCRUD.readAllObjects();
+	}
+
+	@Override
+	public AbstractProduct updateProduct(long id,
+										 AbstractProduct update) {
+		return this.productCRUD.updateObject(update);
 	}
 
 	@Override
 	public boolean deleteProduct(long id) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.productCRUD.deleteObject(id);
 	}
 
 	@Override
-	public IndividualisedProductItem readProduct(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public AbstractProduct readProduct(long id) {
+		AbstractProduct prod = (AbstractProduct) this.productCRUD.readObject(id);
+		if (prod != null) {
+			return prod;
+		} else {
+			throw new NotFoundException("The touchpoint with id " + id + " does not exist!");
+		}
 	}
 	
 }
